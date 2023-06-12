784eb261b21726f1eb5754f59f4e2e08e1a453ea
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-06-12 04:32:43 +0000

    Implement find key by vale

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(x, y) {
+  let emptyArray = Object.keys(x);
+  for (let i of emptyArray) {
+          if(x[i] === y) {
+              return i
+          }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
