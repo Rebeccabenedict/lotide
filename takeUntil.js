6b54ac486fdc58dd63fc10629f32050d14ca07a1
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-06-18 03:14:14 +0000

    Implement take until

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,5 @@
+// The reason why the function below is used is because we want it to return a slice with the function inputted in the parameter 'calback'.
+
 const takeUntil = function(array, callback) {
   let outcome = [];
   for (let i in array) {
@@ -6,8 +8,9 @@ const takeUntil = function(array, callback) {
       }
   }
   return outcome;
-}
+};
 
+/*
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -17,3 +20,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+*/
+
+module.exports = takeUntil;
