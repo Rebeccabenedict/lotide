a5d5f0d17e2d800da05069afafa2b97d4073c385
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-06-18 03:10:10 +0000

    Implement find key by value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// The reason why the function below is used is because we want it to return the identical item among the parameters 'x' and 'y'.
 
 const findKeyByValue = function(x, y) {
   let emptyArray = Object.keys(x);
@@ -15,11 +9,15 @@ const findKeyByValue = function(x, y) {
   }
 };
 
+/*
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+*/
+
+module.exports = findKeyByValue;
