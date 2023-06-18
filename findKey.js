eb5d0bcdfedaed9feb5d6570a47687f659013ccb
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-06-18 03:09:19 +0000

    Implement find key

--- findKey.js
+++ findKey.js
@@ -1,3 +1,7 @@
+/* The reason why the function below is used is because we want it to take an 'object' and return the key of the location which was
+given in the 'callback' parameter. 
+*/
+
 const findKey = function(object, callback) {
   let emptyObject = Object.keys(object);
   for (let i of emptyObject) {
@@ -7,11 +11,15 @@ const findKey = function(object, callback) {
   }
 };
 
-console.log(findKey({
+/*
+findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) )
+}, x => x.stars === 2) // => "noma"
+*/
+
+module.exports = findKey;
