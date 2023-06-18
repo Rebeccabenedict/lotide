0f7c81d44570e120180cc12bede2dc5a1a68c084
Rebecca Benedict Anton <rebecca.benedictanton@gmail.com>
2023-06-18 03:12:16 +0000

    Implement join list

--- joinList.js
+++
@@ -1,9 +0,0 @@
-const joinList = function(list) {
-  let newList = [];
-  for (let x = 0; x < list.length; x++) {
-    newList.push(list[x]);
-  }
-  console.log(`Today i learned about ${newList.join(', ')}.`);
-}
-
-console.log(joinList(["gists", "types", "operators", "iteration", "problem solving"]));
